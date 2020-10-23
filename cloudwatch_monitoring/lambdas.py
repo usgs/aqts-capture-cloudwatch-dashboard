8d355be0174a194a6ccc790094f4bb871502ec72
ssoper <ssoper@usgs.gov>
2020-10-23 16:28:56 -0500

    Add deploy stage filtering

--- cloudwatch_monitoring/lambdas.py
+++ cloudwatch_monitoring/lambdas.py
@@ -25,6 +25,7 @@ def create_lambda_widgets(region, deploy_stage):
     # initialize a list of lambda widgets
     lambda_widgets = []
 
+    # First, create generic widgets for each lambda tagged 'IOW' in the account.
     # get all the lambdas in the account
     all_lambdas_response = lambda_client.list_functions(MaxItems=1000)
 
@@ -34,50 +35,52 @@ def create_lambda_widgets(region, deploy_stage):
         # this is the name we specify in each lambda's serverless.yml config file.
         function_name = function['FunctionName']
 
-        # separate API call to grab metadata for a specific function, specifically we are interested in the tags
-        function_metadata = lambda_client.get_function(FunctionName=function_name)
+        # first, we can limit some upcoming API calls by filtering on deploy tier
+        if deploy_stage.upper() in function_name:
 
-        # we only want lambdas that are tagged as 'IOW'
-        if 'Tags' in function_metadata:
-            tags = function_metadata['Tags']
-            if 'wma:organization' in tags:
-                if 'IOW' == tags['wma:organization']:
+            # Launch a new API call to grab metadata for a specific function, specifically we are interested in the tags
+            function_metadata = lambda_client.get_function(FunctionName=function_name)
 
-                    # Not sure what SC-807615458658-pp-7rv6atnjwitc6-D-CleanupFunction-1LIPB7Y7PMUWB is, filter it out
-                    if 'CleanupFunction' not in function_name:
+            # we only want lambdas that are tagged as 'IOW'
+            if 'Tags' in function_metadata:
+                tags = function_metadata['Tags']
+                if 'wma:organization' in tags:
+                    if 'IOW' == tags['wma:organization']:
+                        # Not sure what SC-807615458658-pp-7rv6atnjwitc6-D-CleanupFunction-1LIPB7Y7PMUWB is, filter it out
+                        if 'CleanupFunction' not in function_name:
 
-                        # generic widget template taken from existing custom dashboard
-                        widget = {
-                            'type': 'metric',
-                            'x': x,
-                            'y': y,
-                            'height': lambda_widget_height,
-                            'width': lambda_widget_width,
-                            'properties': {
-                                "metrics": [
-                                    ["AWS/Lambda", "ConcurrentExecutions", "FunctionName", function_name],
-                                    [".", "Invocations", ".", ".", {"stat": "Sum"}],
-                                    [".", "Duration", ".", "."],
-                                    [".", "Errors", ".", ".", {"stat": "Sum"}],
-                                    [".", "Throttles", ".", "."]
-                                ],
-                                "view": "singleValue",
-                                "region": region,
-                                "title": function_name,
-                                "period": 300,
-                                "stacked": False,
-                                "stat": "Average"
+                            # generic widget template taken from existing custom dashboard
+                            widget = {
+                                'type': 'metric',
+                                'x': x,
+                                'y': y,
+                                'height': lambda_widget_height,
+                                'width': lambda_widget_width,
+                                'properties': {
+                                    "metrics": [
+                                        ["AWS/Lambda", "ConcurrentExecutions", "FunctionName", function_name],
+                                        [".", "Invocations", ".", ".", {"stat": "Sum"}],
+                                        [".", "Duration", ".", "."],
+                                        [".", "Errors", ".", ".", {"stat": "Sum"}],
+                                        [".", "Throttles", ".", "."]
+                                    ],
+                                    "view": "singleValue",
+                                    "region": region,
+                                    "title": function_name,
+                                    "period": 300,
+                                    "stacked": False,
+                                    "stat": "Average"
+                                }
                             }
-                        }
 
-                        # add each widget to the widget list
-                        lambda_widgets.append(widget)
+                            # add each widget to the widget list
+                            lambda_widgets.append(widget)
 
-                        # iterate the position on the dashboard for the next widget
-                        x += lambda_widget_width
-                        if x + lambda_widget_width > lambda_widget_max_width:
-                            x = 0
-                            y += lambda_widget_height
+                            # iterate the position on the dashboard for the next widget
+                            x += lambda_widget_width
+                            if x + lambda_widget_width > lambda_widget_max_width:
+                                x = 0
+                                y += lambda_widget_height
 
     # Custom widget for monitoring concurrency of lambdas specifically involved in the ETL
     concurrent_lambdas = {
