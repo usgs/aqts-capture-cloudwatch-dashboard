8ecfff09aaa83c084806599e9c0fa5fcbdac0d6d
ssoper <ssoper@usgs.gov>
2020-10-27 12:23:16 -0500

    breaking up large function

--- cloudwatch_monitoring/lambdas.py
+++ cloudwatch_monitoring/lambdas.py
@@ -5,6 +5,52 @@ module for creating lambda widgets
 import boto3
 
 
+def get_all_lambda_metadata(region):
+    """
+    Using the AWS python sdk (boto3), grab all the lambda functions for the specified account for a given region.
+
+    :param region: The region, for us that's usually us-west-2
+    :return: response: dict containing a list of metadata about each lambda in the account.
+    """
+    lambda_client = boto3.client("lambda", region_name=region)
+    response = lambda_client.list_functions(MaxItems=1000)
+
+    return response
+
+
+def is_iow_asset_filter(function, deploy_stage, region):
+    """
+    Apply filters to determine if the function is a tagged IOW asset in the correct tier.
+
+    :param function: A single lambda function's metadata
+    :param deploy_stage: The specified deployment environment (DEV, TEST, QA, PROD-EXTERNAL)
+    :param region: typically 'us-west-2'
+    :return: boolean
+    """
+    lambda_client = boto3.client("lambda", region_name=region)
+
+    # this is the name we specify in each lambda's serverless.yml config file.
+    function_name = function['FunctionName']
+
+    # set initial boolean
+    is_iow_lambda = False
+
+    # filtering on deploy tier
+    if deploy_stage.upper() in function_name:
+
+        # launch API call to grab metadata for a specific function, we are interested in the tags
+        function_metadata = lambda_client.get_function(FunctionName=function_name)
+
+        # we only want lambdas that are tagged as 'IOW'
+        if 'Tags' in function_metadata:
+            if 'wma:organization' in function_metadata['Tags']:
+                if 'IOW' == function_metadata['Tags']['wma:organization']:
+                    if 'CleanupFunction' not in function_name:
+                        is_iow_lambda = True
+
+    return is_iow_lambda
+
+
 def create_lambda_widgets(region, deploy_stage):
     """
     Iterate over an account's list of lambdas and create generic widgets for those with
@@ -13,8 +59,6 @@ def create_lambda_widgets(region, deploy_stage):
     :return: List of lambda widgets
     :rtype: list
     """
-
-    # create the aws python sdk lambda client
     lambda_client = boto3.client("lambda", region_name=region)
 
     # set starting and default values for lambda widget positioning and dimensions
@@ -22,67 +66,49 @@ def create_lambda_widgets(region, deploy_stage):
     lambda_widget_width, lambda_widget_height = [24, 3]
     lambda_widget_max_width = 24
 
-    # initialize a list of lambda widgets
     lambda_widgets = []
 
-    # First, create generic widgets for each lambda tagged 'IOW' in the account.
-    # get all the lambdas in the account
-    all_lambdas_response = lambda_client.list_functions(MaxItems=1000)
+    # grab all the lambdas in the account/region
+    all_lambda_metadata_response = get_all_lambda_metadata(region)
+    print(all_lambda_metadata_response)
 
-    print(all_lambdas_response)
+    # iterate over the list of lambda metadata and create widgets for the assets we care about based on filters
+    for function in all_lambda_metadata_response['Functions']:
 
-    # we are only interested in a subset of lambdas in the account
-    for function in all_lambdas_response['Functions']:
+        if is_iow_asset_filter(function, deploy_stage, region):
 
-        # this is the name we specify in each lambda's serverless.yml config file.
-        function_name = function['FunctionName']
+            function_name = function['FunctionName']
 
-        # first, we can limit some upcoming API calls by filtering on deploy tier
-        if deploy_stage.upper() in function_name:
+            widget = {
+                'type': 'metric',
+                'x': x,
+                'y': y,
+                'height': lambda_widget_height,
+                'width': lambda_widget_width,
+                'properties': {
+                    "metrics": [
+                        ["AWS/Lambda", "ConcurrentExecutions", "FunctionName", function_name],
+                        [".", "Invocations", ".", ".", {"stat": "Sum"}],
+                        [".", "Duration", ".", "."],
+                        [".", "Errors", ".", ".", {"stat": "Sum"}],
+                        [".", "Throttles", ".", "."]
+                    ],
+                    "view": "singleValue",
+                    "region": region,
+                    "title": function_name,
+                    "period": 300,
+                    "stacked": False,
+                    "stat": "Average"
+                }
+            }
 
-            # Launch a new API call to grab metadata for a specific function, specifically we are interested in the tags
-            function_metadata = lambda_client.get_function(FunctionName=function_name)
+            lambda_widgets.append(widget)
 
-            # we only want lambdas that are tagged as 'IOW'
-            if 'Tags' in function_metadata:
-                tags = function_metadata['Tags']
-                if 'wma:organization' in tags:
-                    if 'IOW' == tags['wma:organization']:
-                        # Not sure what SC-807615458658-pp-7rv6atnjwitc6-D-CleanupFunction-1LIPB7Y7PMUWB is, filter it out
-                        if 'CleanupFunction' not in function_name:
-
-                            # generic widget template taken from existing custom dashboard
-                            widget = {
-                                'type': 'metric',
-                                'x': x,
-                                'y': y,
-                                'height': lambda_widget_height,
-                                'width': lambda_widget_width,
-                                'properties': {
-                                    "metrics": [
-                                        ["AWS/Lambda", "ConcurrentExecutions", "FunctionName", function_name],
-                                        [".", "Invocations", ".", ".", {"stat": "Sum"}],
-                                        [".", "Duration", ".", "."],
-                                        [".", "Errors", ".", ".", {"stat": "Sum"}],
-                                        [".", "Throttles", ".", "."]
-                                    ],
-                                    "view": "singleValue",
-                                    "region": region,
-                                    "title": function_name,
-                                    "period": 300,
-                                    "stacked": False,
-                                    "stat": "Average"
-                                }
-                            }
-
-                            # add each widget to the widget list
-                            lambda_widgets.append(widget)
-
-                            # iterate the position on the dashboard for the next widget
-                            x += lambda_widget_width
-                            if x + lambda_widget_width > lambda_widget_max_width:
-                                x = 0
-                                y += lambda_widget_height
+            # iterate the position on the dashboard for the next widget
+            x += lambda_widget_width
+            if x + lambda_widget_width > lambda_widget_max_width:
+                x = 0
+                y += lambda_widget_height
 
     # Custom widget for monitoring concurrency of lambdas specifically involved in the ETL
     concurrent_lambdas = {
