f41dcfa375ae2933249529a673e6d751c164dac5
ssoper <ssoper@usgs.gov>
2020-11-03 09:46:27 -0600

    first swipe at grabbing RDS instances

--- cloudwatch_monitoring/rds.py
+++ cloudwatch_monitoring/rds.py
@@ -6,5 +6,32 @@ module for creating rds widgets
 import boto3
 
 def create_rds_widgets(region, deploy_stage):
-    # fill in the blanks
-    print('create the rds widgets')
+
+    rds_client = boto3.client("rds", region_name=region)
+
+    """
+    response = client.describe_db_instances(
+        DBInstanceIdentifier='string',
+        Filters=[
+            {
+                'Name': 'string',
+                'Values': [
+                    'string',
+                ]
+            },
+        ],
+        MaxRecords=123,
+        Marker='string'
+        )
+    """
+
+    rds_metadata = rds_client.describe_db_instances(
+        Filters=[{
+            'Name': 'wma:organization',
+            'Values': [
+                'IOW'
+            ]
+        }]
+    )
+
+    print(rds_metadata)
