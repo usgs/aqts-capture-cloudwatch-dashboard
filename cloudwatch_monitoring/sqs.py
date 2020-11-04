f23a2da324e17cff6e58ee3738d6228d04244ea1
ssoper <ssoper@usgs.gov>
2020-11-04 14:52:08 -0600

    see if we have queues...

--- cloudwatch_monitoring/sqs.py
+++ cloudwatch_monitoring/sqs.py
@@ -4,6 +4,7 @@ module for creating sqs widgets
 """
 
 import boto3
+from .lookups import sqs_queues
 
 
 def create_sqs_widgets(region, deploy_stage, positioning):
@@ -24,7 +25,42 @@ def create_sqs_widgets(region, deploy_stage, positioning):
     # iterate over the list of queue urls and create widgets for the assets we care about based on filters
     for queue_url in all_sqs_queue_urls_response['QueueUrls']:
         if is_iow_queue_filter(queue_url, deploy_stage, region):
-            print(queue_url)
+
+            # incoming queue url: https://us-west-2.queue.amazonaws.com/579777464052/aqts-capture-error-queue-TEST
+            # we want the queue name after the last "/"
+            url_parts = queue_url.split('/')
+            queue_name = url_parts[-1]
+
+            tier_agnostic_queue_name = queue_name.replace(f"-{deploy_stage}", '')
+            queue_title = sqs_queues[tier_agnostic_queue_name]['title']
+
+            queue_widget = {
+                'type': 'metric',
+                'x': positioning.x,
+                'y': positioning.y,
+                'height': positioning.height + 3,
+                'width': positioning.max_width,
+                'properties': {
+                    "metrics": [
+                        [ "AWS/SQS", "ApproximateNumberOfMessagesVisible", "QueueName", queue_name],
+                        [ ".", "ApproximateAgeOfOldestMessage", ".", ".", {"yAxis": "right"}],
+                        [ ".", "NumberOfMessagesReceived", ".", ".", {"stat": "Sum"}],
+                        [ ".", "NumberOfMessagesSent", ".", ".", {"stat": "Sum"}],
+                        [".", "NumberOfMessagesDeleted", ".", "."],
+                        [".", "ApproximateNumberOfMessagesDelayed", ".", "."]
+                    ],
+                    "view": "timeSeries",
+                    "stacked": False,
+                    "region": region,
+                    "period": 60,
+                    "title": queue_title,
+                    "stat": "Average",
+
+                }
+            }
+
+        sqs_widgets.append(queue_widget)
+        positioning.iterate_positioning()
 
     return sqs_widgets
 
@@ -79,7 +115,6 @@ def is_iow_queue_filter(queue_url, deploy_stage, region):
 
     # filtering on deploy tier, which we capitalize
     if deploy_stage.upper() in queue_url:
-        print("is the correct tier")
         # launch API call to grab the tags for the queue
         queue_tags = sqs_client.list_queue_tags(QueueUrl=queue_url)
 
