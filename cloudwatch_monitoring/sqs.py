c65629fa0d0bff42b4403234e839ddac95d99c57
ssoper <ssoper@usgs.gov>
2020-11-04 13:39:48 -0600

    grab the queue tags, print the queues we're interested in

--- cloudwatch_monitoring/sqs.py
+++ cloudwatch_monitoring/sqs.py
@@ -20,9 +20,11 @@ def create_sqs_widgets(region, deploy_stage, positioning):
 
     # grab all the sqs queue urls in the account/region
     all_sqs_queue_urls_response = get_all_sqs_queue_urls(region)
-    print(all_sqs_queue_urls_response)
 
-    # TODO do stuff
+    # iterate over the list of queue urls and create widgets for the assets we care about based on filters
+    for queue_url in all_sqs_queue_urls_response:
+        if is_iow_queue_filter(queue_url, deploy_stage, region):
+            print(queue_url)
 
     return sqs_widgets
 
@@ -59,3 +61,32 @@ def get_all_sqs_queue_urls(region):
             break
 
     return response
+
+
+def is_iow_queue_filter(queue_url, deploy_stage, region):
+    """
+    Apply filters to determine if the queue is a tagged IOW asset in the correct tier.
+
+    :param queue_url: A single queue's url
+    :param deploy_stage: The specified deployment environment (DEV, TEST, QA, PROD-EXTERNAL)
+    :param region: typically 'us-west-2'
+    :return: is_iow_queue: is this an IOW queue or not
+    :rtype: bool
+    """
+    sqs_client = boto3.client("sqs", region_name=region)
+
+    is_iow_queue = False
+
+    # filtering on deploy tier, which we capitalize
+    if deploy_stage.upper() in queue_url:
+
+        # launch API call to grab the tags for the queue
+        queue_tags = sqs_client.list_queue_tags(QueueUrl=queue_url)
+
+        # we only want queues that are tagged as 'IOW'
+        if 'Tags' in queue_tags:
+            if 'wma:organization' in queue_tags['Tags']:
+                if 'IOW' == queue_tags['Tags']['wma:organization']:
+                    is_iow_queue = True
+
+    return is_iow_queue
